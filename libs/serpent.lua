2768fe4edf0d0529f0d03cb3af6fea28b732f6e0
Paydar <omid.paydar1818@gmail.com>
2017-10-08 21:48:06 +0330

    PaydarTeam

--- libs/serpent.lua
+++ libs/serpent.lua
@@ -17,7 +17,7 @@ local function s(t, opts)
   local iname, comm = '_'..(name or ''), opts.comment and (tonumber(opts.comment) or math.huge)
   local seen, sref, syms, symn = {}, {'local '..iname..'={}'}, {}, 0
   local function gensym(val) return '_'..(tostring(tostring(val)):gsub("[^%w]",""):gsub("(%d%w+)",
-    -- tostring(val) is needed because __tostring may return a non-string value
+    -- tostring(val) is needed because __tostring may return a non-string value 
     function(s) if not syms[s] then symn = symn+1; syms[s] = symn end return tostring(syms[s]) end)) end
   local function safestr(s) return type(s) == "number" and tostring(huge and snum[tostring(s)] or s)
     or type(s) ~= "string" and tostring(s) -- escape NEWLINE/010 and EOF/026
